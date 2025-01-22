3572fe2184673c3e9c392daadc182de1258cf1a2
shraddha429-fe <shraddha.singh02@outlook.com>
2025-01-22 23:20:24 +0530

    Added Dropdown component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Dropdown from './Components/Dropdown/Dropdown'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const onMenuItemClick = (id: string) => {
+    console.log(">> id: ", id);
+  }
 
+  const menuItems = [
+    {
+      title: "Sort by episode, asc",
+      id: "1",
+    },
+    {
+      title: "Sort by episode, desc",
+      id: "2",
+    },
+    {
+      title: "Sort by year, asc",
+      id: "3"
+    },
+    {
+      title: "Sort by year, desc",
+      id: "4"
+    }
+  ];
+
+  const buttonText = "Sort By";
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Dropdown 
+        onMenuItemClick={onMenuItemClick}
+        menuItems={menuItems}
+        buttonText={buttonText}
+      />
     </>
   )
 }
---
+++ src/Components/Dropdown/Dropdown.tsx
@@ -0,0 +1,54 @@
+import { SyntheticEvent, useState } from 'react';
+import './Dropdown.css';
+
+interface MenuItem {
+    title: string;
+    id: string;
+}
+
+interface Props {
+    buttonText: string;
+    menuItems: MenuItem[];
+    onMenuItemClick : (id: string) => void;
+}
+
+const Dropdown = (props : Props) => {
+    const [open, setOpen] = useState(false);
+
+    const { buttonText, menuItems, onMenuItemClick} = props;
+    const [selected, setSelected] = useState();
+
+    const handleMenuChange = (e: SyntheticEvent) => {
+        const {id} = e.target;
+        setSelected(id);
+        onMenuItemClick(id);
+        setOpen((open) => !open);
+    }
+
+    const handleOpen = () => {
+        setOpen((open) => !open);
+    }
+    return(
+        <div className="container">
+            <button className="dropdownBtn" onClick={handleOpen}>{buttonText}</button>
+            {open && 
+                <div className='menu'>
+                    {menuItems.map((item) => {
+                        return (
+                            <div 
+                                id={item.id} 
+                                onClick={handleMenuChange} 
+                                key={item.id} 
+                                className={`menuItem ${selected === item.id ? 'selected' : ''}`}
+                            >
+                                {item.title}
+                            </div>
+                        )
+                    })}
+                </div>
+            }
+        </div>
+    )
+};
+
+export default Dropdown;
